dd93685358d633a6d03b735c6eb094f9488b75c7
Aleksandr Kryachko <aleksan.kryachko@gmail.com>
2020-06-21 09:35:28 +0700

    Перенес locators.py в папку Pages

--- test_login_page.py
+++ test_login_page.py
@@ -2,7 +2,7 @@
 pytest -v --tb=line --language=en test_login_page.py
 
 """
-from pages.login_page import LoginPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
